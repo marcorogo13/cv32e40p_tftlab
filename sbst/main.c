bcb53bc3387340bb3276d63fd329e790c9103040
ROSA GOBBO MARCO <s309578@po.polito.it>
2024-01-23 22:26:38 +0100

    finito le simulazioni

--- sbst/main.c
+++ sbst/main.c
@@ -11,7 +11,7 @@ int main()
 	fails = 0;
 	signature_t signatures[NUMTESTS];
 
-	int test0_iterations = 56;
+	int test0_iterations = 28;
 	int test1_iterations = 28;
 	int test2_iterations = 56;
 
@@ -20,16 +20,16 @@ int main()
 	int test4_iterations = 28;
 	int test5_iterations = 0;
 	
-	// signatures[TEST1] = test1(test0_iterations);
+	signatures[TEST1] = test1(test0_iterations);
 	// signatures[TEST2] = test2(test1_iterations);
 	// signatures[TEST3] = test3(test2_iterations);
 	//signatures[TEST4] = test4(test3_iterations);
-	signatures[TEST5] = test5(test4_iterations);
+	//signatures[TEST5] = test5(test4_iterations);
 	//signatures[TEST6] = test6(test5_iterations);
 
-	// if (signatures[TEST1] != test0_iterations){
-	// 	fails++;
-	// }
+	if (signatures[TEST1] != test0_iterations){
+		fails++;
+	}
 	// if (signatures[TEST2] != test1_iterations){
 	// 	fails++;
 	// }
@@ -39,9 +39,9 @@ int main()
 	// if (signatures[TEST4] != test3_iterations){
 	// 	fails++;
 	// }
-	if (signatures[TEST5] != test4_iterations){
-		fails++;
-	}
+	// if (signatures[TEST5] != test4_iterations){
+	// 	fails++;
+	// }
 	// if (signatures[TEST6] != test5_iterations){
 	// 	fails++;
 	// }	
